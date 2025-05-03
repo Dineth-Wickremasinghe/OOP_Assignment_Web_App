20f63d77b337fd6c2442c0c5223089a6573ed49d
IT24100442 <it24100442@my.sliit.lk>
2025-05-03 07:50:44 +0530

    Added Customer Delete Functionality Added File: DeleteCustomerServlet.java

--- src/main/java/org/example/oop_assignment_web_app/Boundary/DeleteCustomerServlet.java
+++ src/main/java/org/example/oop_assignment_web_app/Boundary/DeleteCustomerServlet.java
@@ -1,21 +1,20 @@
 package org.example.oop_assignment_web_app.Boundary;
 
-import jakarta.servlet.*;
+
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-import java.io.IOException;
+
 import org.example.oop_assignment_web_app.Control.CustomerManager;
+
+import java.io.IOException;
 
 @WebServlet(name = "DeleteCustomerServlet", value = "/DeleteCustomerServlet")
 public class DeleteCustomerServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
         String username = request.getParameter("oldUsername");
         CustomerManager cm = new CustomerManager();
         cm.deleteCustomer(username);
