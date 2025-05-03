72967032b5b986a2f47d713cbdac8caaebffdc3c
IT24100442 <it24100442@my.sliit.lk>
2025-05-03 07:39:22 +0530

    Added Customer Edit Functionality

--- src/main/java/org/example/oop_assignment_web_app/Boundary/EditCustomerServlet.java
+++ src/main/java/org/example/oop_assignment_web_app/Boundary/EditCustomerServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.oop_assignment_web_app.Control.CustomerManager;
+import org.example.oop_assignment_web_app.Entity.Customer;
 
 import java.io.IOException;
 
@@ -17,6 +19,26 @@ public class EditCustomerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-            //code to be added
+        String oldUsername = request.getParameter("oldUsername");
+        String oldPassword = request.getParameter("oldPassword");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        try {
+            Customer c;
+            if(!password.isEmpty()) {
+                c = new Customer(oldUsername, email, password);
+            }
+            else{
+                c = new Customer(oldUsername, email, oldPassword);
+            }
+            CustomerManager cm = new CustomerManager();
+            cm.editCustomer(c);
+            response.sendRedirect("myCustomerAccount.jsp");
+        }
+        catch (Exception e) {
+            System.out.println("Error in EditCustomerServlet");
+            response.sendRedirect("editCustomer.jsp");
+        }
     }
 }
